2b66f4dfb089fa523517dad95b5cfe31277a2641
EduGollner0609 <dudugollner@gmail.com>
2025-07-12 20:11:32 -0300

    Criando backend.

---
+++ backend/src/main/java/com/eduardo/avaliacao_calezani/AvaliacaoCalezaniApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.avaliacao_calezani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoCalezaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoCalezaniApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduardo/avaliacao_calezani/AvaliacaoCalezaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.avaliacao_calezani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoCalezaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
